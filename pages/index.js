332786c068f0bb8fd8d99f25247740fabe35bc29
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-01-06 21:50:14 -0300

    tag

--- pages/index.js
+++ pages/index.js
@@ -8,40 +8,47 @@ import { RichText } from "prismic-reactjs";
 import { Client } from "../utils/prismicHelpers";
 import useUpdatePreviewRef from '../utils/useUpdatePreviewRef';
 
+import { Date } from 'prismic-reactjs';
+import { format } from 'date-fns-tz'
+
 
 const Home = ({posts}) => {
 
+	// const date = Date(posts[0].data.date)
+	// const displayDate = format(date,'dd, MMMM, yyyy');
 
-	console.log(posts)
+	console.log()
 
     return (
-    <main style={{maxWidth: "1000px", margin: "auto"}}> 
-      <h1>teste Home page</h1>
-      <h2>Categorias</h2>
-      <NextLink href="/categories/front-end">
-      	<a>Front-end</a>
-      </NextLink>
-      <NextLink href="/categories/back-end">
-      	<a>Back-end</a>
-      </NextLink>
-      <h3>blog original</h3>
-      <NextLink href="/blog">
-      	<a>link para blog</a>
-      </NextLink>
-      <h2>Teste de posts:</h2>
+	    <main style={{maxWidth: "1000px", margin: "auto"}}> 
+	      <h1>teste Home page</h1>
+	      <h2>Categorias</h2>
 
-      {posts.map((post) => (
-      	<div key={post.id} style={{display: "flex"}}>
-      		<div style={{display: "bloq"}}>
-      			<h3>{RichText.asText(post.data.title)}</h3>
-      			<p>{post.data.date}</p>
-      			<p>{RichText.asText(post.data.resume)}</p>
-      		</div>
-      		<img src={post.data.capa.url} alt="" style={{width: "200px"}}/>
-        </div>
-      ))}
+	      <NextLink href="/tag/front-end">
+	      	<a>Front-end</a>
+	      </NextLink>
+	      <NextLink href="/tag/back-end">
+	      	<a>Back-end</a>
+	      </NextLink>
+	      <h3>blog original</h3>
+	      <NextLink href="/blog">
+	      	<a>link para blog</a>
+	      </NextLink>
 
-    </main>
+	      <h2>Teste de posts:</h2>
+	      
+	      {posts.map((post) => (
+	      	<div key={post.id} style={{display: "flex"}}>
+	      		<div style={{display: "bloq"}}>
+	      			<h3>{RichText.asText(post.data.title)}</h3>
+	      			<p>{post.data.date}</p>
+	      			<p>{RichText.asText(post.data.resume)}</p>
+	      		</div>
+	      		<img src={post.data.capa.url} alt="" style={{width: "200px"}}/>
+	        </div>
+	      ))}
+
+	    </main>
     );
   }
 ;
